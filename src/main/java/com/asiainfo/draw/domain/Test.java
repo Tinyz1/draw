52e6f0a49247c29521b9c518b128414ae5de5f34
yecl <yecl@asiainfo.com>
2016-01-05 12:30:07 +0800

    Signed-off-by: yecl <yecl@asiainfo.com>

---
+++ src/main/java/com/asiainfo/draw/domain/Test.java
@@ -0,0 +1,23 @@
+package com.asiainfo.draw.domain;
+
+public class Test {
+    private Integer tId;
+
+    private String tName;
+
+    public Integer gettId() {
+        return tId;
+    }
+
+    public void settId(Integer tId) {
+        this.tId = tId;
+    }
+
+    public String gettName() {
+        return tName;
+    }
+
+    public void settName(String tName) {
+        this.tName = tName == null ? null : tName.trim();
+    }
+}
---
+++ src/main/java/com/asiainfo/draw/service/TestService.java
@@ -0,0 +1,9 @@
+package com.asiainfo.draw.service;
+
+import com.asiainfo.draw.domain.Test;
+
+public interface TestService {
+	
+	public void save(Test test);
+
+}
