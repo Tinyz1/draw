85b67bcb0fe9020b8246e79205e40ddd0eb507ca
yecl <yecl@asiainfo.com>
2016-01-19 17:17:19 +0800

    最终版

---
+++ src/main/java/com/asiainfo/draw/aop/ExecuteLogger.java
@@ -0,0 +1,45 @@
+package com.asiainfo.draw.aop;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * ÷¥––»’÷æ
+ * 
+ * @author yecl
+ *
+ */
+@Component
+@Aspect
+public class ExecuteLogger {
+
+	private final Logger logger = LoggerFactory.getLogger(ExecuteLogger.class);
+
+	@Pointcut("execution(* com.asiainfo.draw.service.*Service*.*(..))")
+	public void logger() {
+	}
+
+	@Around("logger()")
+	public void around(ProceedingJoinPoint pjp) throws Throwable {
+		MethodSignature signature = (MethodSignature) pjp.getSignature();
+		Method method = signature.getMethod();
+		logger.info("{}.{}({})", pjp.getTarget().getClass().getName(), method.getName(), Arrays.toString(pjp.getArgs()));
+		try {
+			Object rtn = pjp.proceed();
+			logger.info(String.valueOf(rtn));
+		} catch (Throwable e) {
+			logger.error(e.toString());
+			throw e;
+		}
+	}
+
+}
